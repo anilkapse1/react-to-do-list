558b90d035de4a974d10c3fdd3984384f81fb77e
anilkapse1 <mr.anilkapse@gmail.com>
2021-02-20 10:51:54 +0530

    base on time greeting is changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const d = new Date();
+let time = 15;
+let greeting = "good morning";
+const ctmColor = {};
+
+if(time >=1 && time < 12){
+  greeting = "good morning!"
+  ctmColor.color="red";
+}
+else if(time >=12 && time < 19 ){
+  greeting="good afternoon!";
+  ctmColor.color="blue";
+}
+else{
+  greeting="good night!";
+  ctmColor.color="orange";
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <div>
+    <h1>greeting! <span style={ctmColor}>{greeting}</span></h1>
+  </div>,
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
