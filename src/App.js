b541a8d9ee68ae60b8869fa7363c7a2cf8cc3797
anilkapse1 <mr.anilkapse@gmail.com>
2021-02-20 17:58:17 +0530

    role chane for profile

--- src/App.js
+++ src/App.js
@@ -8,18 +8,18 @@ function App() {
     <div class="container">
       <div class="ctmGrid">
         <Header imgsrc="https://cdn.pixabay.com/photo/2019/08/09/21/20/james-dean-4395893_960_720.jpg"
-          heroname="anil kapse"
-          description="bahut bada actor"
+          heroname="Anil Kapse"
+          description="UI developer"
         />
 
         <Header imgsrc="https://cdn.pixabay.com/photo/2019/05/16/16/50/man-4207514_960_720.jpg"
-          heroname="ramesh kapse"
-          description="bahut bada actor"
+          heroname="Rohit Gupta"
+          description="Sr. Software Engineer"
         />
 
         <Header imgsrc="https://cdn.pixabay.com/photo/2016/01/20/23/10/bowie-1152551_960_720.png"
-          heroname="harshl kapse"
-          description="bahut bada actor"
+          heroname="Jobin Verma"
+          description="Tech Lead"
         />
 
       </div>
